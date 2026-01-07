71c468812cf00d679c09793df5342cbb63efd710
pavandasari1 <pavan.dasari@newfold.com>
2026-01-06 23:16:17 -0500

    Fix API routing: ensure catch-all route works for all HTTP methods

--- api/[...path].js
+++ api/[...path].js
@@ -3,7 +3,8 @@
 
 const app = require('../server');
 
-// Export as Vercel serverless function
-// Vercel will route all /api/* requests here
+// For Vercel, the catch-all route [...path] means this handles /api/*
+// The path parameter contains the matched segments, but Express routes already include /api
+// So we just export the app directly and it should work
 module.exports = app;
 
